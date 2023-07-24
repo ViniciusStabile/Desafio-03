bd616536127a2fc7de233d4fcce6d31de50607f4
Vinicius Stabile <viniciusstabile@yahoo.com.br>
2023-07-23 23:07:35 -0300

    FindById

--- src/main/java/Desafio03/services/ClientService.java
+++ src/main/java/Desafio03/services/ClientService.java
@@ -1,5 +1,26 @@
 package Desafio03.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import Desafio03.DTO.ClientDTO;
+import Desafio03.entities.Client;
+import Desafio03.repositories.ClientRepository;
+
+@Service
 public class ClientService {
 
+	@Autowired
+	private ClientRepository repository;
+	
+	
+	@Transactional(readOnly = true)
+	public ClientDTO findById(@PathVariable Long id) {
+		Client client = repository.findById(id).get();
+		return new ClientDTO(client);
+	}
+	
+	
 }
