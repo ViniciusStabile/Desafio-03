766d2022534af96b4a85d7122efdd853521a7399
Vinicius Stabile <viniciusstabile@yahoo.com.br>
2023-07-24 16:34:27 -0300

    FindAll service, update Service, insert service, delete Service

--- src/main/java/Desafio03/services/ClientService.java
+++ src/main/java/Desafio03/services/ClientService.java
@@ -1,7 +1,10 @@
 package Desafio03.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -14,13 +17,49 @@ public class ClientService {
 
 	@Autowired
 	private ClientRepository repository;
-	
-	
+
+	@Transactional(readOnly = true)
+	public List<ClientDTO> findAll() {
+		List<Client> list = repository.findAll();
+		return list.stream().map(x -> new ClientDTO(x)).toList();
+	}
+
 	@Transactional(readOnly = true)
 	public ClientDTO findById(@PathVariable Long id) {
 		Client client = repository.findById(id).get();
 		return new ClientDTO(client);
 	}
+
+	
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		CopyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+	}
+	
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		Client client = repository.getReferenceById(id);
+		CopyDtoToEntity(dto, client);
+		client = repository.save(client);
+		return new ClientDTO(client);
+	}
+	
+	@Transactional(propagation = Propagation.SUPPORTS)
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
 	
 	
+	
+	
+	public void CopyDtoToEntity(ClientDTO dto, Client client) {
+		client.setCpf(dto.getCpf());
+		client.setName(dto.getName());
+		client.setIncome(dto.getIncome());
+		client.setBirthDate(dto.getBirthDate());
+		client.setChildren(dto.getChildren());
+	}
 }
